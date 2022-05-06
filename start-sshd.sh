a0b3616b4799d0a58d3a6e7a0992d73c50ebc4eb
Markus Weigelt <Markus.Weigelt@slub-dresden.de>
2022-05-06 17:46:40 +0200

    uncomment imklog in /etc/rsyslog.conf

--- start-sshd.sh
+++ start-sshd.sh
@@ -10,6 +10,10 @@ echo ocrd:x:$UID:$GID:SSH user:/:/bin/bash >> /etc/passwd
 echo ocrd:*:19020:0:99999:7::: >> /etc/shadow
 #/usr/sbin/sshd -D -e
 service ssh start
+
+# Replace imklog to prevent starting problems of rsyslog
+/bin/sed -i '/imklog/s/^/#/' /etc/rsyslog.conf
+
 service rsyslog start
 sleep 1
 tail -f /var/log/syslog
