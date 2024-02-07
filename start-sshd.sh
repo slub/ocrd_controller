5fcf80677778ff47a5e6c0f1ea1acecbde9bf909
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2024-02-07 02:12:02 +0100

    startup: avoid repeating file actions

--- start-sshd.sh
+++ start-sshd.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+# avoid repeating file actions when restarting container:
+if ! grep -q ^ocrd: /etc/passwd; then
 
 # copy the mounted credentials into the user dir
 cat /authorized_keys >> /.ssh/authorized_keys
@@ -7,7 +9,7 @@ mkdir -p $TESSDATA_PREFIX
 cp /usr/local/share/tessdata/*.traineddata $TESSDATA_PREFIX/
 
 # silence the greeting
-touch /.hushlogin
+> /.hushlogin
 
 # re-use most of the environment from root (i.e. Dockerfile)
 set | fgrep -ve BASH > /.ssh/environment
@@ -28,16 +30,18 @@ echo admin:*:19020:0:99999:7::: >> /etc/shadow
 # also, we cannot use $$ directly, because SSHRC is not sourced but execd
 # (so instead, we use the parent of the parent PID)
 echo 'test x$USER != xocrd && exit' >> /.ssh/rc
-echo 'parent=$(cat /proc/$PPID/stat | cut -d\  -f4)' >> /.ssh/rc
+echo 'parent=$(ps -o ppid= $PPID)' >> /.ssh/rc
 echo "workers=${WORKERS:-1}" >> /.ssh/rc
 echo 'sem --will-cite -j $workers --bg --id ocrd_controller_job tail --pid $parent -f /dev/null' >> /.ssh/rc
+
+# disable kernel logging to allow unpriviledged rsyslog
+/bin/sed -i '/imklog/s/^/#/' /etc/rsyslog.conf
+
+fi
 
 # start OpenSSH in the background
 #/usr/sbin/sshd -D -e
 service ssh start
-
-# disable kernel logging to allow unpriviledged rsyslog
-/bin/sed -i '/imklog/s/^/#/' /etc/rsyslog.conf
 
 # start Syslog in the background
 service rsyslog start
